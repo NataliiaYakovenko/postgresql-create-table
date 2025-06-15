21b52550a33fdeac350cd499c6377298e1a1b7e0
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-06-15 10:16:49 +0300

    Create table students

--- students.sql
+++ students.sql
@@ -1,2 +1,14 @@
-DROP TABLE students;
-CREATE TABLE students();
+CREATE DATABASE home_work;
+
+CREATE TABLE students(
+    id SERIAL,
+    first_name VARCHAR(64) CHECK(first_name != '') NOT NULL,
+    last_name VARCHAR(64) CHECK(last_name != '') NOT NULL,
+    birthday DATE CHECK(birthday <= current_date) NOT NULL,
+    phone_number CHAR(13) NOT NULL,
+    group VARCHAR(20) CHECK (group ~ '^[A-Za-z]{1,6}-\d{1,6}$'),
+    avg_mark NUMERIC(3,2) CHECK(avg_mark >= 0) NOT NULL,
+    gender VARCHAR(30) CHECK(gender != '') NOT NULL,
+    entered_at SMALLINT CHECK(entered_at <= extract(YEAR FROM CURRENT_DATE)) NOT NULL,
+    department VARCHAR(64) CHECK(department != '') NOT NULL
+);
